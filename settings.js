a9ab668022f01c748f32e1e9844fda4ef9cda6bb
Kau854684 <kaustavlaha706@gmail.com>
2025-04-07 13:47:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Kaustav, // Your name
+  botOwner: 'Kaustav', // Your name
   ownerNumber: '917439832771', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
